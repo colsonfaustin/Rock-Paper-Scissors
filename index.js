ad194313c1fe66fb788cd7f2e7cecfb6fea7fb74
Colson <colsonfaustin7@gmail.com>
2026-04-16 20:50:48 -0400

    create functions to get computer and human choices

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+const getComputerChoice = function(){
+    // Get a random number from 0-2
+    const ComputerChoice = Math.floor(Math.random() * 3);
+    let choice = "";
+    if (ComputerChoice === 0) {
+        choice = "rock";
+    } else if (ComputerChoice === 1) {
+        choice = "paper";
+    } else if ( ComputerChoice === 2) {
+        choice = "scissors";
+    }
+    return choice;
+};
+
+// functiont to get human input
+const getHumanChoice = function() {
+    let HumanChoice = prompt("rock,paper,scissors?");
+    return HumanChoice;
+}
+
+
+// Declare the players score variable \
